a078d177bbd74f64536e1d6639876910d878f58b
visitnagendra <visitnagendra>
2016-12-15 11:02:27 +0530

    First commit

---
+++ api/controllers/TestingmodelController.js
@@ -0,0 +1,11 @@
+/**
+ * TestingmodelController
+ *
+ * @description :: Server-side logic for managing testingmodels
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
